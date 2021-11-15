b4d5b1dad034b6752521bab36986d20baa7cc923
Anne Pier <gerbenam@metropolia.fi>
2021-11-15 19:18:47 +0200

    created splashscreen

--- app/src/main/java/TerrainProfile.kt
+++ app/src/main/java/TerrainProfile.kt
@@ -14,9 +14,9 @@ class TerrainProfile(distance: Int, angle: Int) {
     }
 }
 
-class DifficultyLevel(name: String, profiles: List<TerrainProfile>) {
+class DifficultyLevel(name: String, profiles: List<TerrainProfile>?) {
     val name: String
-    val profiles: List<TerrainProfile>
+    val profiles: List<TerrainProfile>?
 
     init {
         this.name = name
@@ -29,14 +29,14 @@ object DifficultyLevels {
 
     init {
         difficultyLevels.add(DifficultyLevel("Beginner", listOf(baseProfile, baseProfile, baseProfile)))
-        difficultyLevels.add(DifficultyLevel("Warm up"))
-        difficultyLevels.add(DifficultyLevel("Easy"))
-        difficultyLevels.add(DifficultyLevel("Endurance"))
-        difficultyLevels.add(DifficultyLevel("Strength"))
-        difficultyLevels.add(DifficultyLevel("Power"))
-        difficultyLevels.add(DifficultyLevel("Athlete"))
-        difficultyLevels.add(DifficultyLevel("Pro Athlete"))
-        difficultyLevels.add(DifficultyLevel("Superhuman"))
-        difficultyLevels.add(DifficultyLevel("Conqueror"))
+        difficultyLevels.add(DifficultyLevel("Warm up", null))
+        difficultyLevels.add(DifficultyLevel("Easy", null))
+        difficultyLevels.add(DifficultyLevel("Endurance", null))
+        difficultyLevels.add(DifficultyLevel("Strength", null))
+        difficultyLevels.add(DifficultyLevel("Power", null))
+        difficultyLevels.add(DifficultyLevel("Athlete", null))
+        difficultyLevels.add(DifficultyLevel("Pro Athlete", null))
+        difficultyLevels.add(DifficultyLevel("Superhuman", null))
+        difficultyLevels.add(DifficultyLevel("Conqueror", null))
     }
 }
--- app/src/main/java/network/util/Constants.kt
+++ app/src/main/java/network/util/Constants.kt
@@ -1,9 +1,8 @@
 package network.util
 
 class Constants {
-
     companion object {
-        const val BASE_URL = "http://192.168.0.5:8800/"
+        const val BASE_URL = "http://87.92.135.142:8800/"
         const val REQ_PACKET_NUM = "1"
         const val REQUEST = "request"
     }
