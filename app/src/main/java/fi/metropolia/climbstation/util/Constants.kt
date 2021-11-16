53e126abb1077eac293beefce81f88bebf9bec1f
Minji Choi <comg2843@gmail.com>
2021-11-17 01:24:51 +0200

    add constant values

--- app/src/main/java/fi/metropolia/climbstation/util/Constants.kt
+++ app/src/main/java/fi/metropolia/climbstation/util/Constants.kt
@@ -1,10 +1,21 @@
-package fi.metropolia.climbstation.fi.metropolia.climbstation.network.util
+package fi.metropolia.climbstation.util
 
 class Constants {
 
     companion object {
         const val BASE_URL = "http://192.168.0.5:8800/"
         const val REQ_PACKET_NUM = "1"
+        const val SERIAL_NUM = "20110001"
         const val REQUEST = "request"
+        const val BEGINNER_MODE = "Beginner"
+        const val WARM_UP_MODE = "Warm up"
+        const val EASY_MODE = "Easy"
+        const val ENDURANCE_MODE = "Endurance"
+        const val STRENGTH_MODE = "Strength"
+        const val POWER_MODE = "Power"
+        const val ATHLETE_MODE = "Athlete"
+        const val PRO_ATHLETE_MODE = "Pro Athlete"
+        const val SUPERHUMAN_MODE = "Superhuman"
+        const val CONQUEROR_MODE = "Conqueror"
     }
 }
