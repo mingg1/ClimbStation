de49d9b2c809d4909d88fe949df0dfcd7f973029
anjansh <anjan.shakya@metropolia.fi>
2021-11-19 14:51:27 +0200

    database for statistics

---
+++ app/src/main/java/fi/metropolia/climbstation/database/Climb.kt
@@ -0,0 +1,14 @@
+package fi.metropolia.climbstation.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "climb_table")
+data class Climb(
+    @PrimaryKey(autoGenerate = true)
+    val dateTime: Int,
+    val level: String,
+    val length: Int,
+    val duration: Int
+    )
