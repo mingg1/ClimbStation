8ad1788754620c36ea19a76116e644d2532d5a2f
anjansh <anjan.shakya@metropolia.fi>
2021-11-22 10:40:12 +0200

    id value for database

--- app/src/main/java/fi/metropolia/climbstation/database/Climb.kt
+++ app/src/main/java/fi/metropolia/climbstation/database/Climb.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "climb_table")
 data class Climb(
     @PrimaryKey(autoGenerate = true)
+    val uid: Int,
     val dateTime: Int,
     val level: String,
     val length: Int,
