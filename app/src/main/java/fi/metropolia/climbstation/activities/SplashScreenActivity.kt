a23d37f413e0bd8c2d8a8fd424efdc779529ca30
Minji Choi <comg2843@gmail.com>
2021-12-11 19:22:19 +0200

    edit app icon and splasch screen

--- app/src/main/java/fi/metropolia/climbstation/activities/SplashScreenActivity.kt
+++ app/src/main/java/fi/metropolia/climbstation/activities/SplashScreenActivity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this@SplashScreenActivity, QRCodeScannerActivity::class.java))
+//        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+//        Handler(Looper.getMainLooper()).postDelayed(
+//            {
+//                val intent = Intent(this@SplashScreenActivity, QRCodeScannerActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            }, 5000
+//        )
+        startActivity(Intent(this@SplashActivity, QRCodeScannerActivity::class.java))
         finish()
-
     }
 }
